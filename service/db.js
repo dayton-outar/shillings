d23bdc7424e36b70ec27063ad6a48286aee3e775
dayton-outar <dayton.outar@gmail.com>
2022-01-09 15:06:15 -0500

    Preparing data layer to use new procedure

--- service/db.js
+++ service/db.js
@@ -56,6 +56,47 @@ const O8Q = {
         }
 
         return result;
+    },
+    updateCompany: async function ( listing ) {
+        let result = {
+            success: false,
+            message: 'Failed to update company details',
+            data: {}
+        };
+
+        try {
+
+            if ( listing.companies.length === 0) {
+                result.message = 'No data to update database';
+                return result;
+            }
+
+            let pool = await sql.connect(config);
+
+            let companies = xml2json.json2xml(listing, {
+                compact: true,
+                ignoreComment: true,
+                spaces: 4
+            });
+
+            let dbr = await pool.request()
+                .input('companies', sql.Xml, companies)
+                .execute('UpdateCompaniesDetails');
+            
+            console.log(dbr);
+
+            result = {
+                success: dbr.returnValue === 0,
+                message: `Successfully updated ${listing.companies.length} companies`,
+                data: {}
+            };
+
+        } catch(ex) {
+            console.error( ex.message );
+            result.message = ex.message;
+        }
+
+        return result;
     }
 }
 
