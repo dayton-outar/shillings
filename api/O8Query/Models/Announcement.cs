0af1e4c10aacc1b67c99aa88e9a6b9835ef6d9d8
dayton-outar <dayton.outar@gmail.com>
2022-03-08 20:25:17 -0500

    Types of announcements

--- api/O8Query/Models/Announcement.cs
+++ api/O8Query/Models/Announcement.cs
@@ -7,6 +7,43 @@ namespace O8Query.Models
 {
     public class Announcement
     {
+        // Credit: https://www.getsmarteraboutmoney.ca/invest/investment-products/stocks/factors-that-can-affect-stock-prices/
+        public enum Type
+        {
+            /// <summary>
+            /// General meeting
+            /// </summary>
+            Meeting,
+            /// <summary>
+            /// Dividends pay out
+            /// </summary>
+            Dividend,
+            /// <summary>
+            /// Quarterly (10-Q) or Annual (10-K) Report
+            /// </summary>
+            FinancialReport,
+            /// <summary>
+            /// Merger and acquisition
+            /// </summary>
+            Acquisition,
+            /// <summary>
+            /// Change of management
+            /// </summary>
+            Management,
+            /// <summary>
+            /// Organization restructuring; employee layoffs
+            /// </summary>
+            Restructuring,
+            /// <summary>
+            /// New product, product failure or product recall
+            /// </summary>
+            Product,
+            /// <summary>
+            /// Scandal
+            /// </summary>
+            Scandal
+        }
+
         /// <summary>
         /// Uniquely identifies this announcement
         /// </summary>
