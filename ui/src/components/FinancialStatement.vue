970986a5db274b8d9bf974b8dd86105c48047532
dayton-outar <dayton.outar@gmail.com>
2022-04-10 15:56:55 -0500

    Now importing assays

--- ui/src/components/FinancialStatement.vue
+++ ui/src/components/FinancialStatement.vue
@@ -56,36 +56,12 @@
                                     </b-button>
                                 </template>
 
-                                <b-dropdown-item value="0" aria-role="listitem">
-                                    Operating
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="1" aria-role="listitem">
-                                    Direct
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="2" aria-role="listitem">
-                                    Depreciation
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="3" aria-role="listitem">
-                                    Impairment
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="4" aria-role="listitem">
-                                    Interest
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="5" aria-role="listitem">
-                                    Other
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="6" aria-role="listitem">
-                                    Investment
-                                </b-dropdown-item>
-
-                                <b-dropdown-item value="7" aria-role="listitem">
-                                    Tax
+                                <b-dropdown-item 
+                                    aria-role="listitem"
+                                    v-for="ass in getSectionAssays(props.row.section)"
+                                    :key="ass"
+                                    :value="ass">
+                                    {{ formatTitleCase(ass) }}
                                 </b-dropdown-item>
 
                             </b-dropdown>
@@ -257,6 +233,14 @@ export default {
         },
         getState(state) {
             return state === 'Opened';
+        },
+        getSectionAssays(sectionName) {
+            let ias = -1;
+            if (sectionName) {
+                ias = this.assays.findIndex(as => as.sectional.toLowerCase() === sectionName.replace(' ', '_').toLowerCase());
+            }
+
+            return ias > -1 ? this.assays[ias].assay : [];
         },
         openItem(no) {
             const ix = this.statementItems.findIndex(p => p.no === no);
