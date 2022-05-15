32d560bd08e4c00af3f67e1ee7c3060baf4ffdc8
dayton-outar <dayton.outar@gmail.com>
2022-05-15 12:41:25 -0500

    First draft of financial report procedures

--- db/scratch-financial-report.sql
+++ db/scratch-financial-report.sql
@@ -1,51 +1,5 @@
-DECLARE @reports XML = '<FinancialReport><No>0</No><Company><Code>gk</Code><Name>GRACEKENNEDY LIMITED</Name><Industry /><About /><TotalEmployed>0</TotalEmployed><Wiki /><WebSite /><Founded>1779-01-01T05:07:00Z</Founded><CountryCode /><Created>1999-10-04T05:00:00Z</Created><Industries /><Announcements /></Company><Period>2</Period><StatementDate>2009-12-31T05:00:00Z</StatementDate><Log><No>0</No><Type>Announcement</Type><Event>Information</Event><Details>GK Annual Report</Details><Logged>2022-05-08T05:00:00Z</Logged></Log><IsAudited>true</IsAudited><Analytes><StatementAnalyte><No>0</No><Sequence>1</Sequence><Description>Revenues</Description><Type>1</Type><Section>1</Section><Analyte>1</Analyte><Amount>57406415000</Amount></StatementAnalyte><StatementAnalyte><No>0</No><Sequence>1</Sequence><Description>Cash and deposits</Description><Type>2</Type><Section>7</Section><Analyte>69632</Analyte><Amount>10608376000</Amount></StatementAnalyte></Analytes></FinancialReport>';
-DECLARE @logDetails NVARCHAR(MAX) = '';
+DECLARE @report XML = '<FinancialReport><No>4</No><Company><Code>gk</Code><Name>GRACEKENNEDY LIMITED</Name><Industry /><About /><TotalEmployed>0</TotalEmployed><Wiki /><WebSite /><Founded>1779-01-01T05:07:00Z</Founded><CountryCode /><Created>1999-10-04T05:00:00Z</Created><Industries /><Announcements /></Company><Period>2</Period><StatementDate>2009-12-31T05:00:00Z</StatementDate><Log><No>0</No><Type>Announcement</Type><Event>Information</Event><Details>GK Annual Report</Details><Logged>2022-05-08T05:00:00Z</Logged></Log><IsAudited>true</IsAudited><Analytes><StatementAnalyte><No>1</No><Sequence>1</Sequence><Description>Total Revenues</Description><Type>1</Type><Section>1</Section><Analyte>1</Analyte><Amount>57406415000</Amount></StatementAnalyte><StatementAnalyte><No>2</No><Sequence>1</Sequence><Description>Cash and deposits</Description><Type>2</Type><Section>7</Section><Analyte>69632</Analyte><Amount>10608376000</Amount></StatementAnalyte></Analytes></FinancialReport>';
 
-SELECT  @logDetails = d.x.query('Details').value('.', 'nvarchar(max)')
-FROM    @reports.nodes('FinancialReport/Log') d ( x );
+-- EXEC [dbo].[CreateFinancialReport] @report;
 
-SELECT @logDetails;
-
-DECLARE @tblStatements TABLE
-    (
-        [No] BIGINT,
-        [ReportNo] BIGINT NOT NULL,
-        [Sequence] INT NOT NULL,
-        [Description] NVARCHAR(100) NOT NULL,
-        [Type] INT NOT NULL,
-        [Section] INT NOT NULL,
-        [Analyte] INT NOT NULL,
-        [Amount] DECIMAL(18,2) NOT NULL
-    );
-
-SELECT	d.x.query('No').value('.', 'bigint') [No],
-		d.x.query('Company/Code').value('.', 'nvarchar(20)') [CompanyCode],
-        d.x.query('Period').value('.', 'int') [Period],
-        d.x.query('StatementDate').value('.', 'date') [StatementDate],
-        d.x.query('IsAudited').value('.', 'bit') [IsAudited],
-        7 [LogNo]
-FROM    @reports.nodes('FinancialReport') d ( x )
-
-INSERT INTO @tblStatements
-        (
-            [No] ,
-            [ReportNo] ,            
-            [Sequence] ,
-            [Description] ,
-            [Type] ,
-            [Section] ,
-            [Analyte] ,
-            [Amount]
-        )
-SELECT	d.x.query('No').value('.', 'bigint') [No],
-        7 [ReportNo] , -- Make up a number
-        d.x.query('Sequence').value('.', 'int') [Sequence],
-		d.x.query('Description').value('.', 'nvarchar(100)') [Description],
-        d.x.query('Type').value('.', 'int') [Type],
-        d.x.query('Section').value('.', 'nvarchar(100)') [Section], -- Convert to integer equivalent of enum
-        d.x.query('Analyte').value('.', 'nvarchar(100)') [Analyte], -- Convert to integer equivalent of enum
-        d.x.query('Amount').value('.', 'decimal(18,2)') [Amount]
-FROM    @reports.nodes('FinancialReport/Analytes/StatementAnalyte') d ( x );
-
-SELECT *
-FROM @tblStatements;
+EXEC [dbo].[UpdateFinancialReport] @report;
--- db/stocks.sql
+++ db/stocks.sql
@@ -221,6 +221,80 @@ GO
 ALTER TABLE [dbo].[StockIndices] CHECK CONSTRAINT [FK_StockIndices_Logs_LogNo]
 GO
 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FinancialReports](
+	[No] [bigint] IDENTITY(1,1) NOT NULL,
+	[CompanyCode] [nvarchar](20) NOT NULL,
+	[Period] [int] NOT NULL,
+	[StatementDate] [date] NOT NULL,
+	[IsAudited] [bit] NOT NULL,
+	[LogNo] [bigint] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[FinancialReports] ADD  CONSTRAINT [PK_FinancialReports] PRIMARY KEY CLUSTERED 
+(
+	[No] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+CREATE NONCLUSTERED INDEX [IX_FinancialReports_CompanyCode] ON [dbo].[FinancialReports]
+(
+	[CompanyCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_FinancialReports_LogNo] ON [dbo].[FinancialReports]
+(
+	[LogNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[FinancialReports]  WITH CHECK ADD  CONSTRAINT [FK_FinancialReports_Companies_CompanyCode] FOREIGN KEY([CompanyCode])
+REFERENCES [dbo].[Companies] ([Code])
+GO
+ALTER TABLE [dbo].[FinancialReports] CHECK CONSTRAINT [FK_FinancialReports_Companies_CompanyCode]
+GO
+ALTER TABLE [dbo].[FinancialReports]  WITH CHECK ADD  CONSTRAINT [FK_FinancialReports_Logs_LogNo] FOREIGN KEY([LogNo])
+REFERENCES [dbo].[Logs] ([No])
+GO
+ALTER TABLE [dbo].[FinancialReports] CHECK CONSTRAINT [FK_FinancialReports_Logs_LogNo]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[StatementAnalytes](
+	[Sequence] [int] NOT NULL,
+	[Description] [nvarchar](100) NOT NULL,
+	[Section] [int] NOT NULL,
+	[Analyte] [int] NOT NULL,
+	[Amount] [decimal](18, 2) NOT NULL,
+	[Type] [int] NOT NULL,
+	[ReportNo] [bigint] NULL,
+	[No] [bigint] IDENTITY(1,1) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[StatementAnalytes] ADD  CONSTRAINT [PK_StatementAnalytes] PRIMARY KEY CLUSTERED 
+(
+	[No] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_StatementAnalytes_ReportNo] ON [dbo].[StatementAnalytes]
+(
+	[ReportNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[StatementAnalytes] ADD  DEFAULT ((0)) FOR [Type]
+GO
+ALTER TABLE [dbo].[StatementAnalytes]  WITH CHECK ADD  CONSTRAINT [FK_StatementAnalytes_FinancialReports_ReportNo] FOREIGN KEY([ReportNo])
+REFERENCES [dbo].[FinancialReports] ([No])
+GO
+ALTER TABLE [dbo].[StatementAnalytes] CHECK CONSTRAINT [FK_StatementAnalytes_FinancialReports_ReportNo]
+GO
+
 -- Procedures
 SET ANSI_NULLS ON
 GO
@@ -816,6 +890,237 @@ BEGIN
 
     -- DROP TABLE [#tblLogs];
 	DROP TABLE [#tblDividends];
+
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Dayton Outar
+-- Create date: May 15, 2022
+-- Description:	Stores financial report including statement items
+-- =============================================
+CREATE PROCEDURE [dbo].[CreateFinancialReport]
+    @report XML
+AS
+    BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+    --
+    DECLARE @no BIGINT = 0,
+            @logNo BIGINT = 0,
+            @companyCode NVARCHAR(20) = '',
+            @period INT = 0,
+            @statementDate DATE = NULL,
+            @isAudited BIT = NULL,
+            @logDetails NVARCHAR(MAX) = '';
+	
+    SELECT  @logDetails = d.x.query('Details').value('.', 'nvarchar(max)')
+    FROM    @report.nodes('FinancialReport/Log') d ( x );
+
+	SELECT	@companyCode = d.x.query('Company/Code').value('.', 'nvarchar(20)'),
+        	@period = d.x.query('Period').value('.', 'int'),
+        	@statementDate = d.x.query('StatementDate').value('.', 'date'),
+        	@isAudited = d.x.query('IsAudited').value('.', 'bit') --,
+	FROM    @report.nodes('FinancialReport') d ( x );
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        INSERT INTO [dbo].[Logs]
+            (
+                [Details] ,
+                [Event] ,
+                [Type] ,
+                [Logged]
+            )
+            VALUES
+            (
+                @logDetails ,
+                0 ,
+                2 ,
+                GETDATE()
+            );
+        SELECT @logNo = SCOPE_IDENTITY();
+
+        INSERT INTO [dbo].[FinancialReports]
+            (
+                [CompanyCode] ,
+                [Period] ,
+                [StatementDate] ,
+                [IsAudited] ,
+                [LogNo]
+            )
+            VALUES
+            (
+                @companyCode ,
+                @period ,
+                @statementDate ,
+                @isAudited ,
+                @logNo
+            );
+        SELECT @no = SCOPE_IDENTITY();
+
+        INSERT INTO [dbo].[StatementAnalytes]
+            (
+                [ReportNo] ,
+                [Sequence] ,
+                [Description] ,
+                [Type] ,
+                [Section] ,
+                [Analyte] ,
+                [Amount]
+            )
+        SELECT	@no ,
+                d.x.query('Sequence').value('.', 'int') [Sequence],
+                d.x.query('Description').value('.', 'nvarchar(100)') [Description],
+                d.x.query('Type').value('.', 'int') [Type],
+                d.x.query('Section').value('.', 'nvarchar(100)') [Section],
+                d.x.query('Analyte').value('.', 'nvarchar(100)') [Analyte],
+                d.x.query('Amount').value('.', 'decimal(18,2)') [Amount]
+        FROM    @report.nodes('FinancialReport/Analytes/StatementAnalyte') d ( x );
+
+        COMMIT TRANSACTION;
+
+    END TRY
+    BEGIN CATCH
+        
+        ROLLBACK TRANSACTION;
+        THROW;
+
+    END CATCH;
+
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Dayton Outar
+-- Create date: May 15, 2022
+-- Description:	Modifies financial report including statement items
+-- =============================================
+CREATE PROCEDURE [dbo].[UpdateFinancialReport]
+    @report XML
+AS
+    BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+    --
+    DECLARE @no BIGINT = 0,
+            @companyCode NVARCHAR(20) = '',
+            @period INT = 0,
+            @statementDate DATE = NULL,
+            @isAudited BIT = NULL;
+        
+    DECLARE @tblStatements TABLE
+    (
+        [No] BIGINT,
+        [ReportNo] BIGINT NOT NULL,
+        [Sequence] INT NOT NULL,
+        [Description] NVARCHAR(100) NOT NULL,
+        [Type] INT NOT NULL,
+        [Section] INT NOT NULL,
+        [Analyte] INT NOT NULL,
+        [Amount] DECIMAL(18,2) NOT NULL
+    );
+
+	SELECT	@no = d.x.query('No').value('.', 'bigint'),
+            @companyCode = d.x.query('Company/Code').value('.', 'nvarchar(20)'),
+        	@period = d.x.query('Period').value('.', 'int'),
+        	@statementDate = d.x.query('StatementDate').value('.', 'date'),
+        	@isAudited = d.x.query('IsAudited').value('.', 'bit')
+	FROM    @report.nodes('FinancialReport') d ( x );
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+    
+        UPDATE f SET
+            f.[CompanyCode] = @companyCode ,
+            f.[Period] = @period ,
+            f.[StatementDate] = @statementDate ,
+            f.[IsAudited] = @isAudited
+        FROM [dbo].[FinancialReports] f
+        WHERE f.[No] = @no;
+
+        INSERT INTO @tblStatements
+            (
+                [ReportNo] ,
+                [No] ,            
+                [Sequence] ,
+                [Description] ,
+                [Type] ,
+                [Section] ,
+                [Analyte] ,
+                [Amount]
+            )
+        SELECT	@no ,
+                d.x.query('No').value('.', 'bigint') [No],
+                d.x.query('Sequence').value('.', 'int') [Sequence],
+                d.x.query('Description').value('.', 'nvarchar(100)') [Description],
+                d.x.query('Type').value('.', 'int') [Type],
+                d.x.query('Section').value('.', 'int') [Section],
+                d.x.query('Analyte').value('.', 'int') [Analyte],
+                d.x.query('Amount').value('.', 'decimal(18,2)') [Amount]
+        FROM    @report.nodes('FinancialReport/Analytes/StatementAnalyte') d ( x );
+
+        -- Do Updates 
+        UPDATE  i SET     
+            i.[Sequence] = t.[Sequence] ,
+            i.[Description] = t.[Description] ,
+            i.[Type] = t.[Type] ,
+            i.[Section] = t.[Section]
+        FROM    [dbo].[StatementAnalytes] i
+                INNER JOIN @tblStatements t ON i.[No] = t.[No]
+        WHERE   i.[ReportNo] = @no;
+
+        -- Do Deletions
+        DELETE i FROM [dbo].[StatementAnalytes] i
+        WHERE   NOT EXISTS ( SELECT ''
+                            FROM @tblStatements t
+                            WHERE  t.[No] = i.[No] )
+            AND i.[ReportNo] = @no;
+
+        -- Do Insertions
+        INSERT INTO [dbo].[StatementAnalytes]
+            (
+                [ReportNo] ,
+                [Sequence] ,
+                [Description] ,
+                [Type] ,
+                [Section] ,
+                [Analyte] ,
+                [Amount]
+            )
+        SELECT  t.[ReportNo] ,
+                t.[Sequence] ,
+                t.[Description] ,
+                t.[Type] ,
+                t.[Section] ,
+                t.[Analyte] ,
+                t.[Amount]
+        FROM @tblStatements t
+        WHERE   NOT EXISTS ( SELECT ''
+                            FROM [dbo].[StatementAnalytes] i
+                            WHERE i.[No] = t.[No] )
+        AND t.[ReportNo] = @no; 
+
+        COMMIT TRANSACTION;
+
+    END TRY
+    BEGIN CATCH
+        
+        ROLLBACK TRANSACTION;
+        THROW;
+
+    END CATCH;   
 
 END
 GO
