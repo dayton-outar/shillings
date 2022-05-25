f33e7316e910bfd479381ef22f4ba0e76bf0bcf2
dayton-outar <dayton.outar@gmail.com>
2022-05-24 19:14:02 -0500

    Giving credit where it's due

--- api/Harpoon/EnumExtension.cs
+++ api/Harpoon/EnumExtension.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Linq.Expressions;
 
 namespace Harpoon {
-    public static class EnumExtension {
+    public static class EnumExtension 
+    {
+        // Adapted from: https://stackoverflow.com/questions/53636974/c-sharp-method-to-combine-a-generic-list-of-enum-values-to-a-single-value/53637933#53637933
         public static T ToCombined<T>(this IEnumerable<T> list) where T : Enum
         {
             Type underlyingType = Enum.GetUnderlyingType(typeof(T));
