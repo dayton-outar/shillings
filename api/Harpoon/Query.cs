d0e2c035fb7379ea27ce34f65038d1f0087c903a
dayton-outar <dayton.outar@gmail.com>
2022-09-20 19:08:06 -0500

    Get the stocks related to companies

--- api/Harpoon/Query.cs
+++ api/Harpoon/Query.cs
@@ -57,7 +57,7 @@ namespace Harpoon
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Company> GetCompanies([ScopedService]StocksQuery sq) => sq.Companies.Include(c => c.Files);
+        public IQueryable<Company> GetCompanies([ScopedService]StocksQuery sq) => sq.Companies.Include(c => c.Files).Include(c => c.Stocks);
 
         [UseDbContext(typeof(StocksQuery))]
         [UsePaging]
