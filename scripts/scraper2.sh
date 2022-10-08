d1bb2eb4c255bfa3488c82fe7e3e4e0566c0b56a
dayton-outar <dayton.outar@gmail.com>
2022-10-08 10:23:41 -0500

    Filename 08$ upset new version of Node.js

--- scripts/scraper2.sh
+++ scripts/scraper2.sh
@@ -4,4 +4,4 @@ export MSSQLUSER=SA
 export MSSQLPWD=Password44
 
 echo "$(date) ... Reading companies details"
-/usr/local/bin/node /home/dayton/Documents/Projects/shillings/service/o8$.js read-companies
+/usr/local/bin/node /home/dayton/Documents/Projects/shillings/service/main.js read-companies
