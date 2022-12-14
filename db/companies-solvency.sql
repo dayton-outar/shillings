9778ae6d60567b6151330bc0d99106c96ef030a9
dayton-outar <dayton.outar@gmail.com>
2022-12-14 10:15:55 -0500

    Solvency query

--- db/companies-solvency.sql
+++ db/companies-solvency.sql
@@ -3,92 +3,75 @@ DECLARE @start INT = 0,
         @sortBy TINYINT = 2,
         @sortOrder VARCHAR(4) = 'asc';
 
---- SOLVENCY ... INCLUDES ASSETS AND LIABILITIES
--- SELECT    ROW_NUMBER() OVER (ORDER BY CASE WHEN @sortBy = 0
---                                         AND @sortOrder = 'asc' THEN
---                                                 pvtSolvency.[CompanyCode]
---                                     END ASC,
---                                     CASE WHEN @sortBy = 0
---                                         AND @sortOrder = 'desc' THEN
---                                                 pvtSolvency.[CompanyCode]
---                                     END DESC,
---                                     CASE WHEN @sortBy = 1
---                                         AND @sortOrder = 'asc' THEN
---                                                 pvtSolvency.[StatementDate]
---                                     END ASC,
---                                     CASE WHEN @sortBy = 1
---                                         AND @sortOrder = 'desc' THEN
---                                                 pvtSolvency.[StatementDate]
---                                     END DESC,
---                                     CASE WHEN @sortBy = 2
---                                         AND @sortOrder = 'asc' THEN
---                                                 pvtSolvency.[8]
---                                     END ASC,
---                                     CASE WHEN @sortBy = 2
---                                         AND @sortOrder = 'desc' THEN
---                                                 pvtSolvency.[8]
---                                     END DESC) [Index]
-SELECT
-         pvtSolvency.[CompanyCode]
-        , pvtSolvency.[StatementDate] 
-        , pvtSolvency.[8] [Total Assets]
---         , ISNULL(pvtSolvency.[8], 0) [Current Assets]
---         , ISNULL(pvtSolvency.[9], 0) [Current Liabilities]
---         , ISNULL(pvtSolvency.[8], 0) / ISNULL(pvtSolvency.[9], 0) [Ratio] -- Working Capital and Current Ratio
---         , COUNT(*) OVER () AS [Total]
+--- SOLVENCY: Elements for Total Assets, Current Ratio and Debt-to-Equity
+SELECT    totalAssets.[No]
+        , totalAssets.[CompanyCode]
+        , totalAssets.[StatementDate]
+        , totalAssets.[Total]
+        , workingCapital.[Ratio]
+        , debtToEquity.[Ratio]
+FROM
+(SELECT    pvtAssets.[No]
+        , pvtAssets.[CompanyCode]
+        , pvtAssets.[StatementDate] 
+        , pvtAssets.[8] [Total]
 FROM 
 (   
-SELECT    f.[CompanyCode]
+    SELECT    f.[No]
+            , f.[CompanyCode]
             , f.[StatementDate]
             , a.[Section]
-        --     , CASE WHEN (a.[Analyte] & 65536) = 65536 THEN
-        --         1
-        --         ELSE
-        --         0
-        --         END [Current]
-        --     , CASE WHEN (a.[Analyte] & 262144) = 262144 THEN
-        --         1
-        --         ELSE
-        --         0
-        --         END [Debt] --Loan
-        --     , CASE WHEN (a.[Analyte] & 512) = 512 THEN
-        --         1
-        --         ELSE
-        --         0
-        --         END [EquityType] -- Non-controlling
             , SUM(a.[Amount]) [Amount]            
     FROM [stocks].[dbo].[StatementAnalytes] a
         INNER JOIN [stocks].[dbo].[FinancialReports] f ON f.[No] = a.[ReportNo]
     WHERE a.[Type] = 2
-        --AND (a.[Analyte] & 65536) = 65536
-    GROUP BY f.[No], f.[CompanyCode], f.[StatementDate], a.[Section], (a.[Analyte] & 65536), (a.[Analyte] & 262144), (a.[Analyte] & 512)
-    ) solvency -- Elements for Total Assets, Current Ratio and Debt-to-Equity
+    GROUP BY f.[No], f.[CompanyCode], f.[StatementDate], a.[Section]
+) assets
 PIVOT 
 (
-    SUM(solvency.[Amount]) FOR solvency.[Section] IN ( [8] )
-) pvtSolvency
--- --WHERE CompanyCode = 'gk'
--- ORDER BY CASE WHEN @sortBy = 0
---             AND @sortOrder = 'asc' THEN
---                     pvtSolvency.[CompanyCode]
---          END ASC,
---          CASE WHEN @sortBy = 0
---             AND @sortOrder = 'desc' THEN
---                     pvtSolvency.[CompanyCode]
---          END DESC,
---          CASE WHEN @sortBy = 1
---             AND @sortOrder = 'asc' THEN
---                     pvtSolvency.[StatementDate]
---          END ASC,
---          CASE WHEN @sortBy = 1
---             AND @sortOrder = 'desc' THEN
---                     pvtSolvency.[StatementDate]
---          END DESC,
---          CASE WHEN @sortBy = 2
---             AND @sortOrder = 'asc' THEN
---                     pvtSolvency.[8]
---          END ASC,
---          CASE WHEN @sortBy = 2
---             AND @sortOrder = 'desc' THEN
---                     pvtSolvency.[8]
---          END DESC
+    SUM(assets.[Amount]) FOR assets.[Section] IN ( [8] )
+) pvtAssets) totalAssets
+INNER JOIN
+(SELECT    pvtWc.[No]
+        , pvtWc.[CompanyCode]
+        , pvtWc.[StatementDate] 
+        , pvtWc.[8] / pvtWc.[9] [Ratio]
+FROM 
+(   
+    SELECT    f.[No]
+            , f.[CompanyCode]
+            , f.[StatementDate]
+            , a.[Section]
+            , SUM(a.[Amount]) [Amount]            
+    FROM [stocks].[dbo].[StatementAnalytes] a
+        INNER JOIN [stocks].[dbo].[FinancialReports] f ON f.[No] = a.[ReportNo]
+    WHERE a.[Type] = 2
+        AND (a.[Analyte] & 65536) = 65536
+    GROUP BY f.[No], f.[CompanyCode], f.[StatementDate], a.[Section]
+) wc
+PIVOT 
+(
+    SUM(wc.[Amount]) FOR wc.[Section] IN ( [8], [9] )
+) pvtWc) workingCapital ON totalAssets.[No] = workingCapital.[No]
+INNER JOIN
+(SELECT    pvtDe.[No]
+        , pvtDe.[CompanyCode]
+        , pvtDe.[StatementDate] 
+        , ISNULL(pvtDe.[9], 0) / pvtDe.[10] [Ratio]
+FROM 
+(  
+    SELECT    f.[No]
+            , f.[CompanyCode]
+            , f.[StatementDate]
+            , a.[Section]
+            , SUM(a.[Amount]) [Amount]            
+    FROM [stocks].[dbo].[StatementAnalytes] a
+        INNER JOIN [stocks].[dbo].[FinancialReports] f ON f.[No] = a.[ReportNo]
+    WHERE a.[Type] = 2
+        AND ( ( a.[Section] = 9 AND (a.[Analyte] & 262144) = 262144 ) OR ( a.[Section] = 10 AND (a.[Analyte] & 512) = 0 ) )
+    GROUP BY f.[No], f.[CompanyCode], f.[StatementDate], a.[Section]
+) de
+PIVOT 
+(
+    SUM(de.[Amount]) FOR de.[Section] IN ( [9], [10] )
+) pvtDe) debtToEquity ON totalAssets.[No] = debtToEquity.[No]
