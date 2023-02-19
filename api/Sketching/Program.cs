6b1845292623b044a7c4d02a195f28f9dc5f9127
dayton-outar <dayton.outar@gmail.com>
2023-02-19 18:42:29 -0500

    Permissions required to create issue

--- api/Sketching/Program.cs
+++ api/Sketching/Program.cs
@@ -9,15 +9,20 @@ namespace Sketching
     {
         static async Task Main(string[] args)
         {
+            // export GITTKN=yay! or /etc/environment
+            var token = Environment.GetEnvironmentVariable("GITTKN");
+
             var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+            client.Credentials = new Credentials(token);
             // var user = await client.User.Get("dayton-outar");
             // Console.WriteLine("{0} has {1} public repositories - go check out their profile at {2}",
             //     user.Name,
             //     user.PublicRepos,
             //     user.Url);
 
+            // Grant access to user to do pull request so that the user can create issue
             var createIssue = new NewIssue("this is lame");
-            var nIssue = await client.Issue.Create("dayton-outar", "krisys-loan-calculator", createIssue);
+            var nIssue = await client.Issue.Create("dayton-outar", "shillings", createIssue);
 
             // var issuesForOctokit = await client.Issue.GetAllForRepository("dayton-outar", "smpl");
             // foreach(var issue in issuesForOctokit) {
