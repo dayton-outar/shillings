1122bb8647c498992dad689788403b690da164ad
dayton-outar <dayton.outar@gmail.com>
2023-03-12 10:07:48 -0500

    RESTORE instructions that works

--- db/run-initialization.sh
+++
@@ -1,6 +0,0 @@
-# Wait to be sure that SQL Server came up
-sleep 90s
-
-# Run the setup script to create the DB and the schema in the DB
-# Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -q "RESTORE DATABASE [stocks] FROM DISK = N'/var/opt/mssql/bkup/stocks.bak' WITH MOVE 'stocks' TO '/var/opt/mssql/data/stocks.mdf', MOVE 'stocks_log' TO '/var/opt/mssql/data/stocks_log.ldf'"
--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -5,7 +5,7 @@ bkfile="stocks.bak"
 # Backup database
 sqlcmd -S localhost,1401 -U SA -P "Password44" -Q "BACKUP DATABASE [stocks] TO DISK = N'/var/opt/mssql/bkup/$bkfile'"
 cp /var/lib/docker/volumes/sql1data/_data/bkup/$bkfile /home/dayton/Documents/Projects/shillings/db/
-chmod 777 /home/dayton/Documents/Projects/shillings/db/$bkfile
+chmod 777 /home/dayton/Documents/Projects/shillings/db/bkup/$bkfile
 
 # Remove file from container
 docker exec zeus-sql rm /var/opt/mssql/bkup/$bkfile
