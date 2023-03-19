364da482a580a8949cd8becc11d2af094b210674
dayton-outar <dayton.outar@gmail.com>
2023-03-19 17:03:10 -0500

    Latest backup

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -4,7 +4,7 @@ bkfile="stocks.bak"
 
 # Backup database
 sqlcmd -S localhost,1401 -U SA -P "Password44" -Q "BACKUP DATABASE [stocks] TO DISK = N'/var/opt/mssql/bkup/$bkfile'"
-cp /var/lib/docker/volumes/sql1data/_data/bkup/$bkfile /home/dayton/Documents/Projects/shillings/db/
+cp /var/lib/docker/volumes/sql1data/_data/bkup/$bkfile /home/dayton/Documents/Projects/shillings/db/bkup
 chmod 777 /home/dayton/Documents/Projects/shillings/db/bkup/$bkfile
 
 # Remove file from container
