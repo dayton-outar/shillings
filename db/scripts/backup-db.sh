f76267fc22c1fd8198c1456d54ed2bb0d8eb5c82
dayton-outar <dayton.outar@gmail.com>
2024-09-01 08:33:03 -0500

    interest rates up to aug-2024 added

--- db/scripts/backup-db.sh
+++ db/scripts/backup-db.sh
@@ -4,6 +4,7 @@ bkfile="stocks.bak"
 
 # Backup database
 kubectl exec -it mssql-0 -- /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -Q "BACKUP DATABASE [stocks] TO DISK = N'/var/opt/mssql/bkup/$bkfile'"
+# Use secure copy to copy file from container to host if not using kubernetes
 kubectl cp mssql-0:/var/opt/mssql/bkup/stocks.bak /home/dayton/Documents/Projects/shillings/db/bkup/$bkfile
 
 # Remove file from container
