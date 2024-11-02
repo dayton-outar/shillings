615a521e4fc953f846305d0b283ef82860892476
dayton-outar <dayton.outar@gmail.com>
2024-11-02 08:18:19 -0500

    up and running locally #73

--- db/scripts/restore-db.sh
+++ db/scripts/restore-db.sh
@@ -4,7 +4,7 @@
 until /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -Q "SELECT 1" &> /dev/null
 do
     echo "Waiting for SQL Server to start..."
-    sleep 5
+    sleep 15
 done
 
 # Restore the database
